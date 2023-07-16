e82d4fecbc31ab67ac16d9d2a960dd1c9760e6f6
Steve Ernstberger <steve32285@gmail.com>
2023-07-16 10:04:28 -0400

    rendering

--- packages/docs/src/app/app.tsx
+++ packages/docs/src/app/app.tsx
@@ -1,5 +1,33 @@
 import { Header } from 'components';
-import { Button } from '@mui/joy';
+import { Button, Card } from '@mui/joy';
+import { LineChart } from '@mui/x-charts/LineChart';
+
+const ChartCard = () => {
+  return (
+    <Card>
+      <Header
+        title="Chart Card"
+        subtitle="This has a chart in it."
+        action={
+          <Button variant="solid" color="primary">
+            Go
+          </Button>
+        }
+      />
+
+      <LineChart
+        xAxis={[{ data: [1, 2, 3, 5, 8, 10] }]}
+        series={[
+          {
+            data: [2, 5.5, 2, 8.5, 1.5, 5],
+          },
+        ]}
+        width={500}
+        height={300}
+      />
+    </Card>
+  );
+};
 
 export function App() {
   return (
@@ -13,6 +41,8 @@ export function App() {
           </Button>
         }
       />
+
+      <ChartCard />
     </div>
   );
 }

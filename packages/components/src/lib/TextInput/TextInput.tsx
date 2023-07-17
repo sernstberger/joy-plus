dcd405a4bd66c99f6c68230705f39ee4d6662f4f
Steve Ernstberger <steve32285@gmail.com>
2023-07-17 09:18:38 -0400

    added register props

--- packages/components/src/lib/TextInput/TextInput.tsx
+++ packages/components/src/lib/TextInput/TextInput.tsx
@@ -1,13 +1,5 @@
-import { ChartCard, ConnectInput, Form, Header, Stat } from 'components';
-import {
-  Button,
-  Container,
-  FormControl,
-  FormHelperText,
-  FormLabel,
-  Grid,
-  Input,
-} from '@mui/joy';
+import { ConnectInput } from 'components';
+import { FormControl, FormHelperText, FormLabel, Input } from '@mui/joy';
 
 export interface TextInputProps {
   fieldName: string;
@@ -17,7 +9,15 @@ export function TextInput({ fieldName }: TextInputProps) {
   return (
     <ConnectInput>
       {({ register }: any) => (
-        <FormControl {...register(fieldName)}>
+        <FormControl
+          {...register(fieldName, {
+            required: true,
+            max: 2,
+            min: 1,
+            minLength: 2,
+            maxLength: 5,
+          })}
+        >
           <FormLabel>Label</FormLabel>
           <Input placeholder="Placeholder" />
           <FormHelperText>This is a helper text.</FormHelperText>

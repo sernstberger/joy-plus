96a9f31f05ee9b6bbe7292969eae3211e06c5d18
Steve Ernstberger <steve32285@gmail.com>
2023-08-04 06:52:21 -0500

    added material carousel

--- packages/components/src/lib/Carousel/Carousel.tsx
+++ packages/components/src/lib/Carousel/Carousel.tsx
@@ -1,59 +1,104 @@
-import { useRef } from 'react';
-import { Button, Stack } from '@mui/joy';
-import AspectRatio from '@mui/joy/AspectRatio';
-import Box from '@mui/joy/Box';
+import * as React from 'react';
+import { useTheme } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import MobileStepper from '@mui/material/MobileStepper';
+import Sheet from '@mui/joy/Sheet';
+import Typography from '@mui/joy/Typography';
+import Button from '@mui/joy/Button';
+import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft';
+import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
+import SwipeableViews from 'react-swipeable-views';
+import { autoPlay } from 'react-swipeable-views-utils';
+
+const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
 
 export function Carousel({ images }: any) {
-  const carouselRef = useRef<HTMLDivElement>(null);
+  const theme = useTheme();
+  const [activeStep, setActiveStep] = React.useState(0);
+  const maxSteps = images.length;
 
-  const scroll = (scrollOffset: any) => {
-    console.log('scroll', scrollOffset);
-    if (carouselRef.current) {
-      carouselRef.current.scrollLeft += scrollOffset;
-    }
+  const handleNext = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+  };
+
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };
+
+  const handleStepChange = (step: number) => {
+    setActiveStep(step);
   };
 
   return (
-    <div>
-      <Box
+    <Box sx={{ maxWidth: 400, flexGrow: 1 }}>
+      <Sheet
+        // square
+        // elevation={0}
         sx={{
-          width: '100%',
-          overflow: 'auto',
-          // display: 'flex',
-          // gap: 1,
-          scrollSnapType: 'x mandatory',
-
-          '& > *': {
-            scrollSnapAlign: 'center',
-          },
-          '::-webkit-scrollbar': { display: 'none' },
+          display: 'flex',
+          alignItems: 'center',
+          height: 50,
+          pl: 2,
+          bgcolor: 'background.default',
         }}
-        ref={carouselRef}
       >
-        <AspectRatio
-          minHeight={120}
-          // maxHeight={600}
-          maxHeight={'100%'}
-          // objectFit="contain"
-        >
-          {images.map((src: any, index: any) => {
-            const srcSet = `${src}&dpr=2 2x`;
-            return (
-              <img
-                src={src}
-                srcSet={srcSet}
-                loading="lazy"
-                alt={`Car ${index + 1}`}
+        <Typography>{images[activeStep].label}</Typography>
+      </Sheet>
+      <AutoPlaySwipeableViews
+        axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
+        index={activeStep}
+        onChangeIndex={handleStepChange}
+        enableMouseEvents
+      >
+        {images.map((step: any, index: number) => (
+          <div key={step.label}>
+            {Math.abs(activeStep - index) <= 2 ? (
+              <Box
+                component="img"
+                sx={{
+                  height: 255,
+                  display: 'block',
+                  maxWidth: 400,
+                  overflow: 'hidden',
+                  width: '100%',
+                }}
+                src={step.imgPath}
+                alt={step.label}
               />
-            );
-          })}
-        </AspectRatio>
-      </Box>
-      <Stack direction="row" justifyContent="space-between">
-        <Button onClick={() => scroll(-800)}>Prev</Button>
-        <Button onClick={() => scroll(800)}>Next</Button>
-      </Stack>
-    </div>
+            ) : null}
+          </div>
+        ))}
+      </AutoPlaySwipeableViews>
+      <MobileStepper
+        steps={maxSteps}
+        position="static"
+        activeStep={activeStep}
+        nextButton={
+          <Button
+            size="sm"
+            onClick={handleNext}
+            disabled={activeStep === maxSteps - 1}
+          >
+            Next
+            {theme.direction === 'rtl' ? (
+              <KeyboardArrowLeft />
+            ) : (
+              <KeyboardArrowRight />
+            )}
+          </Button>
+        }
+        backButton={
+          <Button size="sm" onClick={handleBack} disabled={activeStep === 0}>
+            {theme.direction === 'rtl' ? (
+              <KeyboardArrowRight />
+            ) : (
+              <KeyboardArrowLeft />
+            )}
+            Back
+          </Button>
+        }
+      />
+    </Box>
   );
 }
 
--- packages/docs/src/app/car.tsx
+++ packages/docs/src/app/car.tsx
@@ -24,9 +24,24 @@ export function Car() {
         <Grid xs={12} sm={12}>
           <Carousel
             images={[
-              'https://images.unsplash.com/photo-1632245889029-e406faaa34cd?auto=format&fit=crop&w=800',
-              'https://images.unsplash.com/photo-1632245872256-49c78a46c400?auto=format&fit=crop&w=800',
-              'https://images.unsplash.com/photo-1632245866516-d9ca09099a2e?auto=format&fit=crop&w=800',
+              {
+                label: 'San Francisco – Oakland Bay Bridge, United States',
+                imgPath:
+                  // 'https://images.unsplash.com/photo-1537944434965-cf4679d1a598?auto=format&fit=crop&w=400&h=250&q=60',
+                  'https://images.unsplash.com/photo-1632245889029-e406faaa34cd?auto=format&fit=crop&w=800',
+              },
+              {
+                label: 'Bird',
+                imgPath:
+                  // 'https://images.unsplash.com/photo-1538032746644-0212e812a9e7?auto=format&fit=crop&w=400&h=250&q=60',
+                  'https://images.unsplash.com/photo-1632245872256-49c78a46c400?auto=format&fit=crop&w=800',
+              },
+              {
+                label: 'Bali, Indonesia',
+                imgPath:
+                  // 'https://images.unsplash.com/photo-1537996194471-e657df975ab4?auto=format&fit=crop&w=400&h=250',
+                  'https://images.unsplash.com/photo-1632245866516-d9ca09099a2e?auto=format&fit=crop&w=800',
+              },
             ]}
           />
         </Grid>

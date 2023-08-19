ea9f4368cccbf687f0bd58c1aa57edd9d538031e
Steve Ernstberger <steve32285@gmail.com>
2023-08-19 09:11:13 -0400

    added inputs

--- packages/biz/src/app/app.tsx
+++ packages/biz/src/app/app.tsx
@@ -2,8 +2,8 @@ import * as React from 'react';
 import { CssVarsProvider } from '@mui/joy/styles';
 import CssBaseline from '@mui/joy/CssBaseline';
 import Box from '@mui/joy/Box';
-import { Sidebar, Nav, Header } from 'components';
-import { Button, Card, Container, Grid } from '@mui/joy';
+import { Sidebar, Nav, Header, Form, TextInput } from 'components';
+import { Button, Card, Container, Divider, Grid, Typography } from '@mui/joy';
 // import MyMessages from './components/MyMessages';
 
 export default function JoyMessagesTemplate() {
@@ -46,6 +46,31 @@ export default function JoyMessagesTemplate() {
               <Grid xs={4}>
                 <Card>aldkfj</Card>
               </Grid>
+            </Grid>
+            <Grid xs={12}>
+              <Divider />
+            </Grid>
+            <Grid xs={12}>
+              <Form>
+                <TextInput
+                  fieldName="businessName"
+                  label="Business name"
+                  required
+                />
+                <TextInput fieldName="effectiveDate" label="Effective date" />
+                {/* industry should be select */}
+                <TextInput fieldName="industry" label="Industry" />
+                {/* address should be a google autocomplete */}
+                <TextInput fieldName="mailingAddress" label="Mailing address" />
+                {/* fein should be a fein field */}
+                <TextInput fieldName="fein" label="FEIN" />
+                <Divider />
+                <Typography>Owner info</Typography>
+                <TextInput fieldName="firstName" label="First name" />
+                <TextInput fieldName="lastName" label="Last name" />
+                <TextInput fieldName="ssn" label="Social security number" />
+                <TextInput fieldName="birthday" label="Birthday" />
+              </Form>
             </Grid>
           </Container>
         </Box>

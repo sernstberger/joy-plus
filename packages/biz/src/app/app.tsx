ca8548d0f7d9c1aaff98359b7e7c58b7339fca4e
Steve Ernstberger <steve32285@gmail.com>
2023-08-22 09:12:10 -0400

    added react router

--- packages/biz/src/app/app.tsx
+++ packages/biz/src/app/app.tsx
@@ -4,17 +4,24 @@ import Box from '@mui/joy/Box';
 import { Sidebar, Nav } from 'components';
 import Home from './home';
 import Flow from './flow';
+import { Route, Routes } from 'react-router-dom';
 
 export default function App() {
   return (
     <CssVarsProvider disableTransitionOnChange>
       <CssBaseline />
+
       <Box sx={{ display: 'flex', minHeight: '100dvh' }}>
         <Nav />
         <Sidebar />
         <Box component="main" className="MainContent" flex={1}>
           {/* <Home /> */}
           <Flow />
+
+          <Routes>
+            <Route path="/" element={<div>home</div>} />
+            <Route path="/foo" element={<div>foo</div>} />
+          </Routes>
         </Box>
       </Box>
     </CssVarsProvider>
